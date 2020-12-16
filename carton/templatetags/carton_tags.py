dc4ce141a8bb7a2fa5e0a970a67449a1c8bebfd6
sherlockth <sherlockth@gmail.com>
2020-12-16 12:57:30 +0200

    added compatibility with django 2.0

--- carton/templatetags/carton_tags.py
+++ carton/templatetags/carton_tags.py
@@ -8,18 +8,19 @@ register = template.Library()
 
 
 def get_cart(context, session_key=None, cart_class=Cart):
-    """
-    Make the cart object available in template.
+	"""
+	Make the cart object available in template.
 
-    Sample usage::
+	Sample usage::
 
-        {% load carton_tags %}
-        {% get_cart as cart %}
-        {% for product in cart.products %}
-            {{ product }}
-        {% endfor %}
-    """
-    request = context['request']
-    return cart_class(request.session, session_key=session_key)
+		{% load carton_tags %}
+		{% get_cart as cart %}
+		{% for product in cart.products %}
+			{{ product }}
+		{% endfor %}
+	"""
+	request = context['request']
+	return cart_class(request.session, session_key=session_key)
 
-register.assignment_tag(takes_context=True, name=CART_TEMPLATE_TAG_NAME)(get_cart)
+# register.assignment_tag(takes_context=True, name=CART_TEMPLATE_TAG_NAME)(get_cart)
+register.simple_tag(takes_context=True, name=CART_TEMPLATE_TAG_NAME)(get_cart)
